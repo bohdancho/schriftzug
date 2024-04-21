3998a032a70a0ff20eb51179462e834bbf274c21
Bohdan Chornokondratenko <bogdan.chornocondratenko@gmail.com>
2024-04-21 19:49:05 +0200

    fix: not-found responsive resizing

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -10,10 +10,10 @@ import {
 
 export default function NotFound() {
     return (
-        <div className='flex items-center justify-center'>
-            <Card className='w-[420px]'>
+        <div className='container flex items-center justify-center'>
+            <Card className='p-2 md:p-8'>
                 <CardHeader className='text-center'>
-                    <CardTitle className='text-4xl lg:text-7xl'>404</CardTitle>
+                    <CardTitle className='text-4xl md:text-7xl'>404</CardTitle>
                     <CardDescription>
                         The page you’re looking for doesn’t exist.
                     </CardDescription>
