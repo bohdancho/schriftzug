64c41668bfecb43d0231b23f77a8d3be55c749b3
Bohdan Chornokondratenko <bogdan.chornocondratenko@gmail.com>
2024-04-21 17:38:29 +0200

    feat: add layout with header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import '~/styles/globals.css'
 
 import { Inter } from 'next/font/google'
 import { Toaster } from '~/components/ui/sonner'
+import { Button } from '~/components/ui/button'
 
 const inter = Inter({
     subsets: ['latin'],
@@ -22,9 +23,21 @@ export default function RootLayout({
     return (
         <html lang='en'>
             <body className={`font-sans ${inter.variable} dark`}>
-                {children}
+                <div className='grid min-h-screen grid-rows-[auto,1fr]'>
+                    <Topnav />
+                    {children}
+                </div>
                 <Toaster />
             </body>
         </html>
+    )
+}
+
+function Topnav() {
+    return (
+        <header className='sticky top-0 flex h-16 items-center justify-between gap-4 border-b bg-background px-4 md:px-6'>
+            <h1 className='text-xl font-bold'>Schriftzug</h1>
+            <Button variant='ghost'>Login</Button>
+        </header>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,10 @@
-'use client'
-
-import { toast } from 'sonner'
 import { Button } from '~/components/ui/button'
 
 export default function HomePage() {
     return (
-        <main className='flex min-h-screen flex-col items-center justify-center gap-4 text-2xl'>
+        <main className='flex flex-col items-center justify-center gap-4 text-2xl'>
             Getting started with Schriftzug
-            <Button onClick={() => toast('Hey! This is a toast.')}>
-                Make a toast
-            </Button>
+            <Button>Purple button</Button>
         </main>
     )
 }
