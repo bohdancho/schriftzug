986937df2563e60069eaf434577dd3586fff101c
Bohdan Chornokondratenko <bogdan.chornocondratenko@gmail.com>
2024-04-21 17:08:50 +0200

    chore: update prettier config

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,12 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 const config = {
-  plugins: ["prettier-plugin-tailwindcss"],
-};
+    plugins: ['prettier-plugin-tailwindcss'],
+    printWidth: 80,
+    tabWidth: 4,
+    semi: false,
+    singleQuote: true,
+    jsxSingleQuote: true,
+    trailingComma: 'all',
+}
 
-export default config;
+export default config
