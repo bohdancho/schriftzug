a39a7e32e83047d792a178ead0d6a408a467b502
Bohdan Chornokondratenko <bogdan.chornocondratenko@gmail.com>
2024-04-21 17:25:10 +0200

    chore: add sonner toasts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
+'use client'
+
+import { toast } from 'sonner'
 import { Button } from '~/components/ui/button'
 
 export default function HomePage() {
     return (
         <main className='flex min-h-screen flex-col items-center justify-center gap-4 text-2xl'>
             Getting started with Schriftzug
-            <Button>Click me</Button>
+            <Button onClick={() => toast('Hey! This is a toast.')}>
+                Make a toast
+            </Button>
         </main>
     )
 }
