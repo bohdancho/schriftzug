2ad772c3e4a48e606537f8a089b7339d44baa3d6
Bohdan Chornokondratenko <bogdan.chornocondratenko@gmail.com>
2024-04-22 10:18:29 +0200

    fix not-found page title size

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,22 +1,14 @@
 import Link from 'next/link'
 import { Button } from '~/components/ui/button'
-import {
-    Card,
-    CardHeader,
-    CardTitle,
-    CardDescription,
-    CardFooter,
-} from '~/components/ui/card'
+import { Card, CardHeader, CardTitle, CardDescription, CardFooter } from '~/components/ui/card'
 
 export default function NotFound() {
     return (
         <div className='container flex items-center justify-center'>
             <Card size='fullPage'>
                 <CardHeader>
-                    <CardTitle>404</CardTitle>
-                    <CardDescription>
-                        The page you’re looking for doesn’t exist.
-                    </CardDescription>
+                    <CardTitle size='lg'>404</CardTitle>
+                    <CardDescription>The page you’re looking for doesn’t exist.</CardDescription>
                 </CardHeader>
                 <CardFooter className='flex justify-center'>
                     <Button asChild>
