5fafa7562ecb70aed67ea4b86ef64402f4055fc8
Bohdan Chornokondratenko <bogdan.chornocondratenko@gmail.com>
2024-04-23 00:10:41 +0200

    create/delete pack for admins

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,13 @@
+import { type User } from '@clerk/nextjs/server'
 import { type ClassValue, clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs))
+}
+
+export function isAdmin(user: User | null) {
+    // TODO: make it use currentUser()
+    // TODO: use clerk roles
+    return !!user?.privateMetadata?.isAdmin
 }
