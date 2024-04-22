5d450d4d5341500f225e5c6d6c9d5b7e157969dd
Bohdan Chornokondratenko <bogdan.chornocondratenko@gmail.com>
2024-04-22 22:42:46 +0200

    add length parameter to words array schema

--- src/server/llm.ts
+++ src/server/llm.ts
@@ -1,15 +1,15 @@
 import { type ZodSchema, z } from 'zod'
 import { env } from '~/env'
 
-export async function generatePackWords(packName: string, amount: number) {
-    console.log(`Generating ${amount} words for pack "${packName}"`)
-    const prompt = getGeneratePackWordsPrompt(packName, amount)
-    const schema = z.array(z.string())
+export async function generatePackWords(name: string, length: number) {
+    console.log(`Generating ${length} words for pack "${name}"`)
+    const prompt = getGeneratePackWordsPrompt(name, length)
+    const schema = z.array(z.string()).length(length)
     return llmRequest(prompt, schema)
 }
 
-function getGeneratePackWordsPrompt(packName: string, amount: number) {
-    return `You are going to be creating a list of words for a board game. The goal of the game is to explain words one after another to the other players, without using same-root words. Make a list of ${amount} words or word groups (no longer than 3 words) with medium explanation difficulty on the following topic: "${packName}". Infer the language from the prompt and use it for the list. Don't write anything but the prompted list, don't preface it with an explanation, no numbers, return a JSON array.`
+function getGeneratePackWordsPrompt(name: string, amount: number) {
+    return `You are going to be creating a list of words for a board game. The goal of the game is to explain words one after another to the other players, without using same-root words. Make a list of ${amount} words or word groups (no longer than 3 words) with medium explanation difficulty on the following topic: "${name}". Infer the language from the prompt and use it for the list. Don't write anything but the prompted list, don't preface it with an explanation, no numbers, return a JSON array.`
 }
 
 const RETRY_MAX_ATTEMPTS = 3
@@ -31,8 +31,8 @@ async function llmRequest<S>(content: string, schema: ZodSchema<S>) {
                     messages: [{ role: 'user', content }],
                 }),
             }).then((res) => res.json() as Promise<LLMResponse>)
-            const outputRaw = res.choices[0]!.message.content
-            const outputUntyped = JSON.parse(outputRaw) as unknown
+            const outputString = res.choices[0]!.message.content
+            const outputUntyped = JSON.parse(outputString) as unknown
             const output = schema.parse(outputUntyped)
             return output
         } catch (e) {
