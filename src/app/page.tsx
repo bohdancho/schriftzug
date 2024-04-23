27ba3340911785ba0828f1471c8241b2a2c07723
Bohdan Chornokondratenko <bogdan.chornocondratenko@gmail.com>
2024-04-23 21:50:36 +0200

    disable delete-pack-button on pending

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,9 @@ import Link from 'next/link'
 import { deletePack, getAllPacks } from '~/server/queries'
 import { currentUser } from '@clerk/nextjs/server'
 import { isAdmin } from '~/lib/utils'
-import { CreatePackForm } from './_create-pack-form'
-import { Trash } from 'lucide-react'
-import { Button } from '~/components/ui/button'
 import { revalidatePath } from 'next/cache'
+import { CreatePackForm } from './_components/_create-pack-form'
+import { DeletePackButton } from './_components/delete-pack-button'
 
 export default async function PackSelectionPage() {
     const packs = await getAllPacks()
@@ -25,9 +24,7 @@ export default async function PackSelectionPage() {
                         }}
                         className='relative flex cursor-pointer flex-col items-center justify-center overflow-clip rounded-md border-2 p-2 text-center text-white shadow-md lg:p-4'
                     >
-                        <Button type='submit' variant='destructive' className='absolute right-1 top-1 p-2'>
-                            <Trash />
-                        </Button>
+                        <DeletePackButton className='absolute right-1 top-1' />
                         <Link href={`/play/${pack.id}`}>
                             <h2 className='p-2 lg:p-4'>{pack.name}</h2>
                         </Link>
